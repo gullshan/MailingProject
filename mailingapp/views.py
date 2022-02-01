efe7900f446efad25cb8c39ec0f32b3969b07148
lokeshrapala <lokesh.rapala@gmail.com>
2022-02-01 19:51:54 +0530

    Initial commit

---
+++ mailingapp/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+import random
+def otp_send(request):
+    subject = 'lokesh it otp'
+    email_message = str(random.randint(100000,999999))
+    From_mail = settings.EMAIL_HOST_USER
+    to_list = ['anilkumarchitturu1432@gmail.com','killerbrijesh@gmail.com',
+'haimantisamanta1997@gmail.com','hareeshmengu888@gmail.com']
+    send_mail(subject,email_message,From_mail,to_list,fail_silently=False)
+    return HttpResponse("otp send successfully")
